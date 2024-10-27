ab6744b111639b7ce1338e3f3dc11a82006dd373
JethroNatividad <natividad.jet@gmail.com>
2024-10-27 20:01:13 +0800

    chore(client): update browserlist

--- client/src/app/(auth)/register/page.js
+++ client/src/app/(auth)/register/page.js
@@ -14,7 +14,9 @@ const Page = () => {
         redirectIfAuthenticated: '/dashboard',
     })
 
-    const [name, setName] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [middleName, setMiddleName] = useState('')
+    const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
@@ -24,7 +26,9 @@ const Page = () => {
         event.preventDefault()
 
         register({
-            name,
+            first_name: firstName,
+            middle_name: middleName,
+            last_name: lastName,
             email,
             password,
             password_confirmation: passwordConfirmation,
@@ -36,19 +40,50 @@ const Page = () => {
         <form onSubmit={submitForm}>
             {/* Name */}
             <div>
-                <Label htmlFor="name">Name</Label>
+                <Label htmlFor="first_name">First Name</Label>
 
                 <Input
-                    id="name"
+                    id="first_name"
                     type="text"
-                    value={name}
+                    value={firstName}
                     className="block mt-1 w-full"
-                    onChange={event => setName(event.target.value)}
+                    onChange={event => setFirstName(event.target.value)}
                     required
                     autoFocus
                 />
 
-                <InputError messages={errors.name} className="mt-2" />
+                <InputError messages={errors.first_name} className="mt-2" />
+            </div>
+
+            <div className="mt-4">
+                <Label htmlFor="middle_name">Middle Name</Label>
+
+                <Input
+                    id="middle_name"
+                    type="text"
+                    value={middleName}
+                    className="block mt-1 w-full"
+                    onChange={event => setMiddleName(event.target.value)}
+                    autoFocus
+                />
+
+                <InputError messages={errors.middle_name} className="mt-2" />
+            </div>
+
+            <div className="mt-4">
+                <Label htmlFor="last_name">Last Name</Label>
+
+                <Input
+                    id="last_name"
+                    type="text"
+                    value={lastName}
+                    className="block mt-1 w-full"
+                    onChange={event => setLastName(event.target.value)}
+                    required
+                    autoFocus
+                />
+
+                <InputError messages={errors.last_name} className="mt-2" />
             </div>
 
             {/* Email Address */}
@@ -86,9 +121,7 @@ const Page = () => {
 
             {/* Confirm Password */}
             <div className="mt-4">
-                <Label htmlFor="passwordConfirmation">
-                    Confirm Password
-                </Label>
+                <Label htmlFor="passwordConfirmation">Confirm Password</Label>
 
                 <Input
                     id="passwordConfirmation"
